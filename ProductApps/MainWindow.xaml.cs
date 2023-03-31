ad0241e136b0234c87b39bd806950263fcc563a1
ozdotcom <shebberman@gmail.com>
2023-03-31 11:47:36 +1030

    commit the branch feat_wrap1.1

--- ProductApps/MainWindow.xaml.cs
+++ ProductApps/MainWindow.xaml.cs
@@ -49,7 +49,7 @@ namespace ProductApps
 
         //    //totalPayment = totalPaymentTextBlock.Text;
             
-        //    //int totalCharge  =  TotalPayment + 25; 
+        //    //int totalCharge  =  TotalPayment + 25 + 5; 
         //    //totalChargeTextBlock.Text = Convert.ToString(delivery.totalPayment);
         //   //return totalCharge;
         //}
